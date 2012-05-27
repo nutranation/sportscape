5d6092d10dcb7564d62b4145b5103220fc31ef7a
Personal <Personal@Spencers-MacBook.local>
2012-05-27 00:10:21 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sportscape::Application.config.secret_token = '127816986b25d5f715d304507edda35673fb37458ce1aac98cfc1f41d32522a942b10c18eb41e78635dd974bbe7355c61f9e9a9dad096dfd2eaceb2383af1989'
