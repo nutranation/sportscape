b54c383eba1fe50d7a13bca5d8316a174a93ab23
Spencer Kline <spencer.kline@gmail.com>
2012-05-27 14:52:54 -0400

    adds work needed for omni auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,18 +8,8 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
-  def self.find_for_facebook_oauth(access_token, signed_in_resource=nil)
-    data = access_token['extra']['user_hash']
-    if user = User.find_by_email(data["email"])
-      unless user.facebook_id
-        user.facebook_id = data["id"] 
-        user.save
-      end
-      user
-    elsif data["email"]  && data["name"] # Create a user with a stub password. 
-      User.create(:email => data["email"], :password => Devise.friendly_token[0,20], :name => data["name"], :facebook_id => data["id"]) 
-    else
-      redirect_to new_user_registration_url
-    end
+  def self.find_for_facebook_oauth(access_token, signed_in_resource = nil)
+    data = access_token.extra.raw_info
+    User.where(:facebook_id => data.id).first
   end
 end
